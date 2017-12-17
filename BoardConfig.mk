b34772f8fd770f537a6881b4463b785b7277458d
bonuzzz <bonuzzz@gmail.com>
2017-12-17 21:25:50 +0300

    public init

---
+++ BoardConfig.mk
@@ -0,0 +1,17 @@
+# Extracted with libbootimg
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/treltexx/dtb.img
+
+# Kernel
+TARGET_KERNEL_CONFIG := cm_exynos5433-trelte_defconfig
+
+# RIL
+BOARD_MODEM_TYPE := m7450
+
+# Recovery
+TARGET_OTA_ASSERT_DEVICE := trelte,treltexx,tre3g,tre3gxx
+
+#BLOCK_BASED_OTA:= false
+LZMA_RAMDISK_TARGETS := recovery
+
+# Inherit common board flags
+include device/samsung/trelte-common/BoardConfigCommon.mk
